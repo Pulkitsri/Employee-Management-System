d299bc1e958ce728c0a55efb691ba5d553f4fbb3
Pulkitsri <pulkit_srivastava99@yahoo.com>
2024-02-12 14:11:42 +0530

    Add Employee Code

--- src/main/java/com/pulkit/EmployeeManagementSystem/Controller/EmpController.java
+++ src/main/java/com/pulkit/EmployeeManagementSystem/Controller/EmpController.java
@@ -3,8 +3,12 @@ package com.pulkit.EmployeeManagementSystem.Controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+
+import com.pulkit.EmployeeManagementSystem.Model.Employee;
 import com.pulkit.EmployeeManagementSystem.Service.EmployeeService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 
 @Controller
@@ -18,5 +22,19 @@ public class EmpController {
     public String viewHomePage(Model model){
         model.addAttribute("listemployees", employeeService.getAllEmployees());
         return "index";
+    }
+
+    @GetMapping("/showNewEmployeeForm")
+    public String showNewEmployeeForm(Model model){
+        //create model attribute to bind from data
+        Employee employee = new Employee();
+        model.addAttribute("employee", employee);
+        return "new_employee";
+    }
+
+    @PostMapping("/saveEmployee")
+    public String saveEmployee(@ModelAttribute("employee") Employee employee){
+        employeeService.addEmployee(employee);
+        return "redirect:/";
     }
 }
